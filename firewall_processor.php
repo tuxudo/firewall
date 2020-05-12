86783494d337ce08c193abc0303e1ae0f3863f91
Jon Crain <joncrain@users.noreply.github.com>
2020-05-12 09:22:47 -0400

    Update firewall_processor.php

--- firewall_processor.php
+++ firewall_processor.php
@@ -8,14 +8,14 @@ class Firewall_processor extends Processor
     public function run($plist)
 	{
         // If plist is empty, echo out error
-		if ( ! $plist){
-			throw new Exception("Error Processing Request: No property list found", 1);
-		}
+        if ( ! $plist){
+                throw new Exception("Error Processing Request: No property list found", 1);
+        }
 
         // Delete previous set
         Firewall_model::where('serial_number', $this->serial_number)->delete();
 
-		$parser = new CFPropertyList();
+        $parser = new CFPropertyList();
         $parser->parse($plist, CFPropertyList::FORMAT_XML);
         $mylist = $parser->toArray();
 
