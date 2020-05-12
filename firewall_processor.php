e041debf74e3218919eeff6a30b920713723921f
Jon Crain <joncrain@users.noreply.github.com>
2020-05-12 09:20:41 -0400

    Update firewall_processor.php
    fixing processor

--- firewall_processor.php
+++ firewall_processor.php
@@ -1,44 +1,28 @@
 <?php
 
+use CFPropertyList\CFPropertyList;
 use munkireport\processors\Processor;
 
 class Firewall_processor extends Processor
 {
-    public function process($plist)
+    public function run($plist)
 	{
         // If plist is empty, echo out error
-        if (! $plist) {
-			echo ("Error Processing firewall module: No data found");
-		} else { 
-
-            $parser = new CFPropertyList();
-            $parser->parse($plist, CFPropertyList::FORMAT_XML);
-            $myList = $parser->toArray();
-
-            $typeList = array(
-                'allowdownloadsignedenabled' => null,
-                'allowsignedenabled' => null,
-                'applications' => null,
-                'firewallunload' => null,
-                'globalstate' => null,
-                'loggingenabled' => null,
-                'loggingoption' => null,
-                'services' => null,
-                'stealthenabled' => null,
-                'version' => null
-            );
-
-            // Process each key
-            foreach ($typeList as $key => $value) {
-                $this->rs[$key] = $value;
-                if(array_key_exists($key, $myList))
-                {
-                    $this->rs[$key] = $myList[$key];
-                }
-            }
-
-            // Save the data, save the MacDevOpsYVR hack night!
-            $this->save();
+		if ( ! $plist){
+			throw new Exception("Error Processing Request: No property list found", 1);
 		}
+
+        // Delete previous set
+        Firewall_model::where('serial_number', $this->serial_number)->delete();
+
+		$parser = new CFPropertyList();
+        $parser->parse($plist, CFPropertyList::FORMAT_XML);
+        $mylist = $parser->toArray();
+
+        $model = Firewall_model::firstOrNew(['serial_number' => $this->serial_number]);
+
+        $model->fill($mylist);
+        $model->save();    
+
 	}
 }
